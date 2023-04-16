6f6d6d62f532cf15a36c554d751fb01cfb61fa73
lolek55507 <110234766+lolek55507@users.noreply.github.com>
2023-04-16 22:45:20 +0100

    Add files via upload

---
+++ function check () {.js
@@ -0,0 +1,15 @@
+let i = 0;
+function check() {
+   
+    if (i === 0) {
+        i++;
+        console.log("i is 0");
+     
+    } else{
+        i--;
+        console.log("i is 1");
+    }
+}
+check();
+check();
+check();
